7dd80461275847cd52d18b4cd31cb2e9511c3c68
BorisK <wss@protonmail.com>
2020-01-13 13:28:51 +0300

    6: Last Commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // Creating the main scene
-var scene = new THREE.Scene();
+window.addEventListener('load', init);
+function init() {
+    var scene = new THREE.Scene();
 
         // Creating a render and setting the size
         var renderer = new THREE.WebGLRenderer({antialias:true});
@@ -9,7 +11,7 @@ var scene = new THREE.Scene();
 
         // Creating a camera with basic position
         var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.set(-30,40,30);
+        camera.position.set(-30,30,40);
         camera.lookAt(scene.position);
 
         // Creating the vertex' coordinates
@@ -33,16 +35,17 @@ var scene = new THREE.Scene();
         ];
 
         let edge=edgeGeometry=[];
-        let sphere= [];
+        let sphere=[];
         let targetList=[];
-        let controls = new function() {
+        var controls = new function() {
             //Creating initial rotation
             this.rotationSpeed = 0.005;
+            this.numberOfObjects = scene.children.length;
 
             //Creating cubes' edges
             this.addCube = function() {
 
-                let cubeCreation=()=> {
+                var cubeCreation=()=> {
                     for (let i = 0; i < cubeEdges.length; i++) {
                         edgeGeometry[i] = new THREE.Geometry();
                         edgeGeometry[i].vertices.push(new THREE.Vector3(cubeEdges[i][0][0], cubeEdges[i][0][1], cubeEdges[i][0][2]));
@@ -50,33 +53,33 @@ var scene = new THREE.Scene();
                         let edgesMaterial = new THREE.LineBasicMaterial({color: 0x000000});
                         edge[i] = new THREE.Line(edgeGeometry[i], edgesMaterial);
                     }
-                    return edge
+                    return edge;
                 }
-
                 // Creating spheres placed on cube's vertexes
-
-                let sphereCreation=()=>{
+                var sphereCreation=()=>{
                     for(let i=0; i<cubeVertex.length; i++) {
-                        sphereGeometry=new THREE.SphereGeometry (0.6,32,32);
+                        var sphereGeometry=new THREE.SphereGeometry (0.6,32,32);
                         var sphereMaterial=new THREE.MeshBasicMaterial({color:  Math.random() * 0xffffff});
                         sphere[i] = new THREE.Mesh(sphereGeometry, sphereMaterial);
                         sphere[i].position.set(cubeEdges[i][0][0], cubeEdges[i][0][1], cubeEdges[i][0][2])
                     }
                     return sphere;
-                }
-                targetList.push(this.sphere);
-                console.log(sphere);
-                console.log(targetList);
-                //Assembling the final object
-                let finalCube=new THREE.Group();
-                    for (let i = 0; i < cubeCreation().length; i++)
-                        finalCube.add( cubeCreation()[i]);
+                };
 
-                    for (let i=0; i < sphereCreation().length; i++)
-                        finalCube.add( sphereCreation()[i]);
-                    finalCube.position.set(Math.random()*20, Math.random()*20, Math.random()*20);
-                    scene.add(finalCube)
-                }
+                // Assembling the final object
+                (function Assembly() {
+                        let finalCube=new THREE.Group();
+                        for (let i = 0; i < cubeCreation().length; i++)
+                            finalCube.add( cubeCreation()[i]);
+
+                        for (let i=0; i < sphereCreation().length; i++)
+                            finalCube.add( sphereCreation()[i]);
+                        finalCube.position.set(Math.random()*20, Math.random()*20,Math.random()*20);
+                        scene.add(finalCube)
+                })()
+                this.numberOfObjects = scene.children.length;
+            };
+
             //Creating remove object function
             this.removeCube = function() {
                 let allChildren = scene.children;
@@ -84,15 +87,19 @@ var scene = new THREE.Scene();
                     var lastObject = allChildren[allChildren.length-1];
                 }
                 scene.remove(lastObject);
+                this.numberOfObjects = scene.children.length;
+            };
+            this.outputObjects = function() {
+                console.log(scene.children);
             }
-
         }
         //Adding GUI elements
         var gui = new dat.GUI();
-
         gui.add(controls, 'addCube');
         gui.add(controls, 'removeCube');
         gui.add(controls, 'rotationSpeed',0,0.05);
+        gui.add(controls, 'outputObjects');
+        gui.add(controls, 'numberOfObjects').listen();
         renderer.render( scene, camera );
 
        // Adding basic animation
@@ -108,3 +115,14 @@ var scene = new THREE.Scene();
             renderer.render(scene, camera)
         };
         animate();
+
+        // var domEvents = new THREEx.DomEvents(camera, renderer.domElement);
+
+        // Window Resize Event
+        window.addEventListener('resize', onWindowResize, false);
+        function onWindowResize() {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            camera.updateProjectionMatrix();
+        };
+}
